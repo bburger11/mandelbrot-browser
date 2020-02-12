ec35df7bf5e36fea3edbdf6ee417109305bd1347
bburger11 <bjburger11@yahoo.com>
2020-02-12 10:31:00 -0800

    add classes for html elements

--- scripts/visualLibrary.js
+++ scripts/visualLibrary.js
@@ -2,47 +2,13 @@ function Item() {
     this.addToDocument = function () { document.body.appendChild(this.myItem); }
 }
 
-function Button() {
-    this.createButton = function(text, id){
-        this.myItem = document.createElement("button");
-        this.myItem.setAttribute("id", id);
-        this.myItem.innerHTML = text;
-    },
-    this.addClickEventHandler = function(handler, args) {
-        this.myItem.onmouseup = function() { handler(args); }
+function Anchor() {
+    this.createAnchor = function(href) {
+        this.myItem = document.createElement("a");
+        this.myItem.setAttribute("href", href);
     }
-}
-
-function Label() {
-    this.createLabel = function(text, id) {
-        this.myItem = document.createElement("p");
-        this.myItem.setAttribute("id", id);
-        this.myItem.appendChild(document.createTextNode(text));
-    },
-    this.setText = function(text) {
-        this.myItem.innerHTML = text;
-    }
-}
-
-function Dropdown() {
-    this.createDropdown = function(dict, id, selected){
-        this.myItem = document.createElement("select");
-        this.myItem.setAttribute("id", id);
-        for (var key in dict) {
-            this.myItem.add(new Option(key));
-        }
-        this.myItem.setAttribute("selected", selected);
-    }, 
-    this.getSelected = function(args){
-        selected = this.myItem.options[this.myItem.selectedIndex].text;
-        return selected;
-    }
-}
-
-function Input() {
-    this.createInput = function(text, id) {
-        this.myItem = document.createElement("input");
-        this.myItem.setAttribute("id", id);
+    this.addToAnchor = function(object) {
+        this.myItem.appendChild(object.myItem);
     }
 }
 
@@ -57,14 +23,25 @@ function Div() {
     }
 }
 
+function Label() {
+    this.createLabel = function(text, id) {
+        this.myItem = document.createElement("p");
+        this.myItem.setAttribute("id", id);
+        this.myItem.appendChild(document.createTextNode(text));
+    },
+    this.setText = function(text) {
+        this.myItem.innerHTML = text;
+    }
+}
+
 function Image() {
     this.createImage = function(src, id) {
         this.myItem = document.createElement("img");
         this.myItem.setAttribute("src", src);
         this.myItem.setAttribute("id", id);
-        this.myItem.style.border = "thick solid #000000";
+        //this.myItem.style.border = "thick solid #000000";
     }
-    this.setSrc = function(src) {
-        this.myItem.setAttribute("src", src);
+    this.addClickEventHandler = function(funcName) {
+        this.myItem.setAttribute("onclick", funcName);
     }
 }
